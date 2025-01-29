94954a9008a36afcd3e67d9adf2ac8cf0a195b78
Hiren <hirenbudhrani@gmail.com>
2025-01-29 11:54:16 +0100

    Make property model, migration, and seeder

---
+++ src/database/factories/PropertyFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Property>
+ */
+class PropertyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->words(3, true),
+            'address' => fake()->address(),
+            'price' => fake()->numberBetween(100000, 2000000),
+        ];
+    }
+}
